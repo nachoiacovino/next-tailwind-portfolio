e5df4c1ac497c026d43a64b00d15889dbcf4f468
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-08-06 11:52:06 +0200

    Rename twitter-card to twitter for cache

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/nachoiacovino/next-tailwind-portfolio',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/twitter.png',
   email: 'nacho.iacovino@gmail.com',
   github: 'https://github.com/nachoiacovino',
   twitter: 'https://twitter.com/nachoiacovino',
